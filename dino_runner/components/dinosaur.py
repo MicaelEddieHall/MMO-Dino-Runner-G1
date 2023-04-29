bda48f7e66bf764c2c0448d8fa78d0f87967342f
MicaelEddieHall <Micael.Miranda@fundacion-jala.org>
2023-04-29 08:13:38 -0400

    Bend-Funtional
    Mi bend esta totalmente funcional

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -9,6 +9,7 @@ from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 JUMP_VELOCITY=8.5
 Y_INITIAL=310
 X_INITIAL=80
+Y_D_DUCKING=30
 
 ##esta heredando sprite a dinosaur
 class dinosaur(Sprite):
@@ -21,12 +22,13 @@ class dinosaur(Sprite):
         self.step=0
         self.action="running"
         self.jump_velocity=JUMP_VELOCITY
+        self.d_ducking=True
 
     def update(self, user_input):
         if self.action=="running":
             self.image=RUNNING[0] if self.step < 5 else RUNNING[1]
             self.step+=1
-        elif self.action=="jumping":
+        elif self.action=="jumping":## and user_input[pygame.K_UP]:
             self.image=JUMPING
             self.rect.y-=self.jump_velocity*4
             self.jump_velocity -=0.8
@@ -37,10 +39,15 @@ class dinosaur(Sprite):
                 self.jump_velocity=JUMP_VELOCITY
         elif self.action=="bend":
             self.image=DUCKING[0] if self.step < 5 else DUCKING[1]
+            if self.d_ducking:
+                self.rect.y+=Y_D_DUCKING
+                self.d_ducking=False
             self.step+=1
-            if self.step==9 and not user_input[pygame.K_DOWN]:
+            if not user_input[pygame.K_DOWN]:
                 self.image=RUNNING[0] if self.step < 5 else RUNNING[1]
                 self.action="running"
+                self.rect.y-=Y_D_DUCKING
+                self.d_ducking=True
 
         if user_input[pygame.K_UP] and self.action!="jumping":
             self.action="jumping"
