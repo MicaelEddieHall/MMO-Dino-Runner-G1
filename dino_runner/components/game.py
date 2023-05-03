a1a4e4c3543f3b880b59079f4a0107482d4a43d5
MicaelEddieHall <Micael.Miranda@fundacion-jala.org>
2023-05-02 22:53:11 -0400

    pantalla negra
    debo iportar otra imagen para el menu, sospecho que ese es el problema

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,10 @@
 import pygame 
+
+from dino_runner.components.score import Score
+
 from dino_runner.components.dinosaur import dinosaur
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, SHIELD
 
 from dino_runner.components.obstaclemanager import ObstacleManager
 
@@ -15,16 +18,29 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
+        self.runing=False
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         
         self.player=dinosaur()
         self.obstacle_manager=ObstacleManager()
+        self.score=Score()
+        self.dead_count=0
 
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
+        while self.playing:
+            if not self.playing:
+                self.show_menu()
+                
+        pygame.quit()
+
+    def play(self):
+        # Game loop: events - update - draw
+        self.playing = True
+        self.obstacle_manager.reset()
         while self.playing:
             self.events()
             self.update()
@@ -35,11 +51,13 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+                self.runing=False
 
     def update(self):
         user_input=pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacle_manager.update(self)
+        self.obstacle_manager.update(self.game_speed,self.player,self.on_death)
+        self.score.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
@@ -47,6 +65,7 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.score.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -58,3 +77,44 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def on_death(self):
+        pygame.time.delay(900)
+        self.playing=False
+        self.dead_count+=1
+
+    def show_menu(self):
+        center_x=SCREEN_WIDTH//2
+        center_y=SCREEN_HEIGHT//2
+        ##cambiar el fondo de pantalla
+        self.screen.fill((255,255,255))
+
+        ##si no hubo muertes mostrar normal, sino mostrar el contador de muertes
+        ##mostrar mensaje de reinicio
+        ##mostrar puntaje obtenido, al parecer las 3 en una misma ventana
+        ##abstraer el codigo de generar texto en medio, para que nos pregunte donde y en que tamaño dibujar
+        
+
+
+        #agregar un texto de inicio en la pantalla
+        font=pygame.font.Font('freesansbold.ttf',30)
+        ##render convierte texto en surface
+        text=font.render("Press any key to start.",True,(0,0,0))
+        text_rect=text.get_rect()
+        text_rect.center=(center_x,center_y)
+        self.screen.blit(text,text_rect)
+        #agregar una imagen en la pantalla
+        self.screen.blit(SHIELD,(center_x-49,center_y-101))
+        ##refrescar pantalla
+        pygame.display.update()
+        #manejar eventos
+        self.handle_menu_events()
+    
+    def handle_menu_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.runing = False
+            elif event.type==pygame.KEYDOWN:
+                self.play()
+
+    
--- dino_runner/components/obstaclemanager.py
+++ dino_runner/components/obstaclemanager.py
@@ -2,32 +2,38 @@ from dino_runner.components.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 import pygame
 import random
+
 class ObstacleManager:
     def __init__(self):
         self.obstacles=[]
         self.list_cactus=[SMALL_CACTUS,LARGE_CACTUS]
-        self.list_tall=[325,310]
+        self.list_tall=[325,300]
         self.cactus=0
         self.Cactus=Cactus(SMALL_CACTUS)
+        ##eso de small cactus puedes hacerlo con lista
+        ##tuplas, desempaquetar y random.choise
 
-    def update(self, game):
-        if len(self.obstacles)<=3:
+    def update(self, game_speed,player,on_death):
+        if len(self.obstacles)<2:
             self.cactus=random.randint(0,1)
             self.Cactus=Cactus(self.list_cactus[self.cactus])
             self.Cactus.rect.y=self.list_tall[self.cactus]
             self.obstacles.append(self.Cactus)
             ##print(len(self.obstacles))
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed,self.obstacles)
+            obstacle.update(game_speed,self.obstacles)
             if(obstacle.rect.x<0):
                 self.obstacles.pop()
             ##print(obstacle.rect.x)
             ##print("se actualizan")
-            if game.player.rect.colliderect(obstacle.rect):
+            if player.rect.colliderect(obstacle.rect):
                 ##detecta las colisiones
-                pygame.time.delay(900)
-                game.playing=False
+                on_death()
 
     def draw(self,screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+    
+    def reset(self):
+        self.obstacles=[]
+        pass
