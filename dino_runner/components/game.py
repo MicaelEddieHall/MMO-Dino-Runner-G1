7338f596ff6cef7f23629c49058344bbd18455b7
MicaelEddieHall <Micael.Miranda@fundacion-jala.org>
2023-05-03 13:14:52 -0400

    Bug al morir
    funciona todo, menos que tengo un bug al morir :,c; hoy preguntare en la sesion de clase para recibir ayuda rapida.

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -4,7 +4,7 @@ from dino_runner.components.score import Score
 
 from dino_runner.components.dinosaur import dinosaur
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, SHIELD
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DINO_START
 
 from dino_runner.components.obstaclemanager import ObstacleManager
 
@@ -18,7 +18,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.runing=False
+        self.running=False
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
@@ -30,10 +30,13 @@ class Game:
 
     def run(self):
         # Game loop: events - update - draw
-        self.playing = True
-        while self.playing:
+        self.running = True
+        while self.running:
             if not self.playing:
                 self.show_menu()
+                print("pucha")
+            else:
+                self.play()
                 
         pygame.quit()
 
@@ -45,13 +48,14 @@ class Game:
             self.events()
             self.update()
             self.draw()
+        print("hmm")
         pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
-                self.runing=False
+                self.running=False
 
     def update(self):
         user_input=pygame.key.get_pressed()
@@ -94,27 +98,45 @@ class Game:
         ##mostrar puntaje obtenido, al parecer las 3 en una misma ventana
         ##abstraer el codigo de generar texto en medio, para que nos pregunte donde y en que tamaño dibujar
         
+        if self.dead_count==0:
+            self.dibuja_texto("Press any key to start",center_x,center_y)
+        else:
+            #agregar un texto de inicio en la pantalla
+            ##font=pygame.font.Font('freesansbold.ttf',30)
+            ##render convierte texto en surface
+            ##text=font.render(f"{self.dead_count}",True,(0,0,0))
+            ##text_rect=text.get_rect()
+            ##text_rect.center=(center_x,center_y)
+            ##self.screen.blit(text,text_rect)
+
+            ##aca usa el dibujar texto 2 veces 
+            self.dibuja_texto(f"{self.dead_count}",center_x,center_y)
+            self.dibuja_texto("Press any key to start",center_x,center_y+50)
 
 
-        #agregar un texto de inicio en la pantalla
-        font=pygame.font.Font('freesansbold.ttf',30)
-        ##render convierte texto en surface
-        text=font.render("Press any key to start.",True,(0,0,0))
-        text_rect=text.get_rect()
-        text_rect.center=(center_x,center_y)
-        self.screen.blit(text,text_rect)
-        #agregar una imagen en la pantalla
-        self.screen.blit(SHIELD,(center_x-49,center_y-101))
+            #agregar una imagen en la pantalla
+
+        self.screen.blit(DINO_START,(center_x-49,center_y-121))            
+        ##desde la 115 hasta la 122 hacerlo en funcion, y pasar el mensaje de press any key 
         ##refrescar pantalla
         pygame.display.update()
         #manejar eventos
         self.handle_menu_events()
+        
     
     def handle_menu_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.runing = False
-            elif event.type==pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                 self.play()
+            elif event.type==pygame.QUIT:
+                self.running=False
+
+    def dibuja_texto(self,texto,center_x,center_y):
+        font=pygame.font.Font('freesansbold.ttf',30)
+        ##render convierte texto en surface
+        text=font.render(texto,True,(0,0,0))
+        text_rect=text.get_rect()
+        text_rect.center=(center_x,center_y)
+        self.screen.blit(text,text_rect)
 
     
--- dino_runner/components/score.py
+++ dino_runner/components/score.py
@@ -6,13 +6,13 @@ class Score:
     def update(self,game):
         self.score+=1
         if self.score%100==0:
-            game.game_speed+=2
+            game.game_speed+=1
     
-    def draw(self):
+    def draw(self,screen):
         font=pygame.font.Font('freesansbold.ttf',22)
         ##render convierte texto en surface
         text=font.render(f"Score: {self.score}",True,(0,0,0))
         text_rect=text.get_rect()
         text_rect.center=(1000,50)
-        self.screen.blit(text,text_rect)
+        screen.blit(text,text_rect)
         
